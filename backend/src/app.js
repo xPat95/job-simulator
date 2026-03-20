f5526ffc7168dc57805b90e9d680627921fcac9d
xPat95 <tol24355@uvg.edu.gt>
2026-03-19 20:13:48 -0600

    pruebas docker up

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,15 @@ const routes = require("./routes");
 const app = express();
 
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("LABORATORIO 4");
+});
+
+app.get("/ping", (req, res) => {
+  res.json({ message: "pong" });
+});
+
 app.use("/", routes);
 
 module.exports = app;
